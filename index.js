faf516bafc109539ca576dde0dfab05da3057431
USER-20240413EW\Administrator <i928625516@sina.com>
2024-05-04 05:54:44 +0800

    -init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+
+app.get('/req', (req, res) => {
+	console.log(req.method);
+	console.log(req.url);
+	console.log(req.httpVersion);
+	console.log(req.headers);
+	
+	console.log(req.path)
+	console.log(req.query)
+	console.log(req.ip)
+	// console.log(req.method);
+	res.send('JSON.stringify(req)')
+})
+
+app.listen(3000, () => {
+	console.log('服务启动。。。')
+})
